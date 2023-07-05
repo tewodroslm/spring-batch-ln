912b32dfe13ebe62d6d761d380971d863a0d65b6
Mamushe <tewodroslemma64@gmail.com>
2023-07-05 08:19:05 -0400

    created a stub Step that a job can start

--- src/main/java/com/ted/springbatchln/SpringBatchLnApplication.java
+++ src/main/java/com/ted/springbatchln/SpringBatchLnApplication.java
@@ -1,10 +1,38 @@
 package com.ted.springbatchln;
 
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class SpringBatchLnApplication {
+
+	@Autowired
+	public JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Step packageItemStep(){
+		return this.stepBuilderFactory.get("packageItemStep").tasklet(new Tasklet() {
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				System.out.println("Item t has been packaged");
+				return RepeatStatus.FINISHED;
+			}
+		}).build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBatchLnApplication.class, args);
