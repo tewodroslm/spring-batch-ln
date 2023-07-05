08d78c77292401a10c48f362cb8b55e8de12bef3
Mamushe <tewodroslemma64@gmail.com>
2023-07-05 07:57:09 -0400

    settup prj

---
+++ src/main/java/com/ted/springbatchln/SpringBatchLnApplication.java
@@ -0,0 +1,13 @@
+package com.ted.springbatchln;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchLnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchLnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ted/springbatchln/SpringBatchLnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ted.springbatchln;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchLnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
